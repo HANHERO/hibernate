76cc4d52d07f3f25b3c6e35af2916d04d6eb7f3f
white-collar <eugen.shmorgun@gmail.com>
2019-04-01 14:54:25 +0300

    initial commit

---
+++ src/main/java/HibernateDemo.java
@@ -0,0 +1,32 @@
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateUtil;
+
+public class HibernateDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Hibernate");
+
+        Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
+        Student student1 = new Student("John", "Cena", "john@javaguides.com");
+
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(student);
+            session.save(student1);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+
+    }
+}
